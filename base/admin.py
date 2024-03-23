d5b883cd0362c6d9bd18ababdcc101caa6c1edc8
Dhan-Bdr-Karki <dhanbdrkarki111@gmail.com>
2024-03-23 21:13:51 +0545

    customized admin panel user

--- base/admin.py
+++ base/admin.py
@@ -3,8 +3,27 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Room, Topic, Message, User
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff') 
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups') 
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('username', 'first_name', 'last_name', 'bio', 'avatar')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}), 
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'first_name', 'last_name', 'password1', 'password2', 'is_staff', 'is_superuser')
+        }),
+    )
+    search_fields = ('email', 'username', 'first_name', 'last_name') 
+    ordering = ('email',) 
+
+
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Room)
 admin.site.register(Topic)
 admin.site.register(Message)
